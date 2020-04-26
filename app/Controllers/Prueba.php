e677250f2e746eb65d339cb146519c15bcd16736
Pablo <z83.servidor@gmail.com>
2020-04-26 14:18:40 +0200

    z83 1 commit

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,17 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+ echo " esta es la funcion index";
+	}
+  public function test(){
+    echo "esto es la funcion test";
+  }
+	public function test2(){
+    echo "esto es la funcion test2";
+  }
+	//--------------------------------------------------------------------
+
+}
